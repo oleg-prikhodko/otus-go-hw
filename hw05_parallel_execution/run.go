2a56375c30ff860241a6e1fccc020c5c6541d9cf
Oleg Prikhodko <super.oleg.3000@yandex.com>
2026-02-03 17:41:46 +0300

    hw05

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,42 +2,69 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+
+var ErrWorkerCountIncorrect = errors.New("n must be > 0")
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	t := make(chan Task, len(tasks))
-	e := make(chan error, m)
+	if n <= 0 {
+		return ErrWorkerCountIncorrect
+	}
+	if m <= 0 {
+		return ErrErrorsLimitExceeded
+	}
 
+	tasksChan := make(chan Task, len(tasks))
+	quitChan := make(chan struct{})
+	closer := func() { close(quitChan) }
+	var once sync.Once
+	var errCount int
+	var errMutex sync.Mutex
+	var wg sync.WaitGroup
+
+	wg.Add(n)
 	for i := 0; i < n; i++ {
-		go worker(t, e)
+		// worker goroutine
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case <-quitChan:
+					return
+				default:
+				}
+
+				task, moreTasks := <-tasksChan
+				if !moreTasks {
+					return
+				}
+				if err := task(); err != nil {
+					errMutex.Lock()
+					if errCount++; errCount >= m {
+						once.Do(closer)
+					}
+					errMutex.Unlock()
+				}
+			}
+		}()
 	}
+
 	for i := 0; i < len(tasks); i++ {
-		t <- tasks[i]
+		tasksChan <- tasks[i]
 	}
-	close(t)
+	close(tasksChan)
 
-	for i := 0; ; {
-		if i >= m {
-			// todo: stop goroutines
-			return ErrErrorsLimitExceeded
-		}
-		err := <-e
-		if err != nil {
-			i++
-		}
+	wg.Wait()
+
+	if errCount >= m {
+		return ErrErrorsLimitExceeded
 	}
 
-	// Place your code here.
 	return nil
-}
-
-func worker(tasks <-chan Task, errors chan<- error) {
-	for t := range tasks {
-		errors <- t()
-	}
 }
