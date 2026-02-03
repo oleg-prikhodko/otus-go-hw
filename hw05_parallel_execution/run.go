1430d1e0344a8624f4a79a1fe2322081c19b489f
Oleg Prikhodko <super.oleg.3000@yandex.com>
2026-02-03 13:22:08 +0300

    hw05

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,6 +10,34 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
+	t := make(chan Task, len(tasks))
+	e := make(chan error, m)
+
+	for i := 0; i < n; i++ {
+		go worker(t, e)
+	}
+	for i := 0; i < len(tasks); i++ {
+		t <- tasks[i]
+	}
+	close(t)
+
+	for i := 0; ; {
+		if i >= m {
+			// todo: stop goroutines
+			return ErrErrorsLimitExceeded
+		}
+		err := <-e
+		if err != nil {
+			i++
+		}
+	}
+
 	// Place your code here.
 	return nil
+}
+
+func worker(tasks <-chan Task, errors chan<- error) {
+	for t := range tasks {
+		errors <- t()
+	}
 }
