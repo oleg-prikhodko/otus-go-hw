43bae9cc2069ca0823790479f3342ef408ecb6e2
Oleg Prikhodko <o.prikhodko@it-is.ru>
2026-03-24 00:50:07 +0300

    hw06 | cleanup

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -15,61 +15,41 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	return in
 }
 
-//nolint:gocognit
 func wrap(stage Stage, done In) Stage {
 	return func(in In) Out {
-		proxyIn := make(Bi)
-		proxyOut := make(Bi)
+		proxyIn := stageWithDone(in, done)
 		stageOut := stage(proxyIn)
-
-		// inner chan consumer
-		go func() {
-			defer func() {
-				close(proxyIn)
-				drain(in)
-			}()
-			for {
-				select {
-				case <-done:
-					return
-				case val, ok := <-in:
-					if !ok {
-						return
-					}
-					select {
-					case <-done:
-						return
-					case proxyIn <- val:
-					}
-				}
-			}
-		}()
-
-		// outer chan producer
-		go func() {
-			defer func() {
-				close(proxyOut)
-				drain(stageOut)
-			}()
-			for {
-				select {
-				case <-done:
-					return
-				case val, ok := <-stageOut:
-					if !ok {
-						return
-					}
-					select {
-					case <-done:
-						return
-					case proxyOut <- val:
-					}
-				}
-			}
-		}()
+		proxyOut := stageWithDone(stageOut, done)
 
 		return proxyOut
 	}
+}
+
+func stageWithDone(in In, done In) Out {
+	proxy := make(Bi)
+	go func() {
+		defer func() {
+			close(proxy)
+			drain(in)
+		}()
+		for {
+			select {
+			case <-done:
+				return
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-done:
+					return
+				case proxy <- val:
+				}
+			}
+		}
+	}()
+
+	return proxy
 }
 
 func drain(in In) {
