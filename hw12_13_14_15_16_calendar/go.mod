2a272fcd9f9b2d3ba4cffc377027d8357e8082f2
Oleg Prikhodko <o.prikhodko@it-is.ru>
2026-03-31 23:58:06 +0300

    hw13 | add grpc impl

--- hw12_13_14_15_16_calendar/go.mod
+++ hw12_13_14_15_16_calendar/go.mod
@@ -1,11 +1,21 @@
 module github.com/oleg-prikhodko/otus-go-hw/hw12_13_14_15_calendar
 
-go 1.23
+go 1.24.0
+
+toolchain go1.24.4
 
 require (
 	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/lib/pq v1.11.2
+	google.golang.org/grpc v1.79.3
 	google.golang.org/protobuf v1.36.11
 	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
 )
--- hw12_13_14_15_16_calendar/internal/server/grpc/server.go
+++ hw12_13_14_15_16_calendar/internal/server/grpc/server.go
@@ -1,3 +1,164 @@
 package grpc
 
-// todo
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/oleg-prikhodko/otus-go-hw/hw12_13_14_15_calendar/internal/app"
+	"github.com/oleg-prikhodko/otus-go-hw/hw12_13_14_15_calendar/internal/common"
+	pb "github.com/oleg-prikhodko/otus-go-hw/hw12_13_14_15_calendar/internal/server/grpc/proto"
+	"github.com/oleg-prikhodko/otus-go-hw/hw12_13_14_15_calendar/internal/storage"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type Server struct {
+	pb.UnimplementedCalendarServiceServer
+	server *grpc.Server
+	app    *app.App
+	logger common.Logger
+	addr   string
+}
+
+func NewServer(logger common.Logger, app *app.App, addr string) *Server {
+	return &Server{
+		app:    app,
+		logger: logger,
+		addr:   addr,
+	}
+}
+
+func (s *Server) Start(ctx context.Context) error {
+	lis, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		return fmt.Errorf("failed to listen: %w", err)
+	}
+
+	s.server = grpc.NewServer()
+	pb.RegisterCalendarServiceServer(s.server, s)
+
+	s.logger.Info(fmt.Sprintf("starting gRPC server at %s", s.addr))
+
+	go func() {
+		if err := s.server.Serve(lis); err != nil {
+			s.logger.Error(fmt.Sprintf("gRPC server error: %s", err))
+		}
+	}()
+
+	<-ctx.Done()
+	s.logger.Info("shutting down gRPC server")
+
+	s.Stop()
+	return nil
+}
+
+func (s *Server) Stop() {
+	s.server.GracefulStop()
+}
+
+func (s *Server) CreateEvent(ctx context.Context, req *pb.CreateEventRequest) (*pb.CreateEventResponse, error) {
+	ev := storage.Event{
+		Title:        req.Title,
+		Time:         req.Time.AsTime(),
+		Duration:     req.Duration.AsDuration(),
+		Description:  &req.Description,
+		OwnerID:      req.OwnerId,
+		NotifyBefore: parseProtoDuration(req.NotifyBefore),
+	}
+
+	if err := s.app.CreateEvent(ev); err != nil {
+		return nil, err
+	}
+
+	return &pb.CreateEventResponse{Id: ev.ID}, nil
+}
+
+func (s *Server) UpdateEvent(ctx context.Context, req *pb.UpdateEventRequest) (*pb.UpdateEventResponse, error) {
+	ev := storage.Event{
+		ID:           req.Id,
+		Title:        req.Title,
+		Time:         req.Time.AsTime(),
+		Duration:     req.Duration.AsDuration(),
+		Description:  &req.Description,
+		OwnerID:      req.OwnerId,
+		NotifyBefore: parseProtoDuration(req.NotifyBefore),
+	}
+
+	if err := s.app.UpdateEvent(ev); err != nil {
+		return nil, err
+	}
+
+	return &pb.UpdateEventResponse{}, nil
+}
+
+func (s *Server) DeleteEvent(ctx context.Context, req *pb.DeleteEventRequest) (*pb.DeleteEventResponse, error) {
+	if err := s.app.DeleteEvent(req.Id); err != nil {
+		return nil, err
+	}
+
+	return &pb.DeleteEventResponse{}, nil
+}
+
+func (s *Server) ListEventsForDay(ctx context.Context, req *pb.ListEventsRequest) (*pb.ListEventsResponse, error) {
+	events, err := s.app.ListEventsForDay(req.Date.AsTime())
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ListEventsResponse{Events: eventsToProto(events)}, nil
+}
+
+func (s *Server) ListEventsForWeek(ctx context.Context, req *pb.ListEventsRequest) (*pb.ListEventsResponse, error) {
+	events, err := s.app.ListEventsForWeek(req.Date.AsTime())
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ListEventsResponse{Events: eventsToProto(events)}, nil
+}
+
+func (s *Server) ListEventsForMonth(ctx context.Context, req *pb.ListEventsRequest) (*pb.ListEventsResponse, error) {
+	events, err := s.app.ListEventsForMonth(req.Date.AsTime())
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ListEventsResponse{Events: eventsToProto(events)}, nil
+}
+
+func parseProtoDuration(d *durationpb.Duration) *time.Duration {
+	if d == nil {
+		return nil
+	}
+	v := d.AsDuration()
+	return &v
+}
+
+func eventsToProto(events []storage.Event) []*pb.Event {
+	result := make([]*pb.Event, len(events))
+	for i, ev := range events {
+		var description string
+		if ev.Description != nil {
+			description = *ev.Description
+		}
+
+		var notifyBefore *durationpb.Duration
+		if ev.NotifyBefore != nil {
+			notifyBefore = durationpb.New(*ev.NotifyBefore)
+		}
+
+		result[i] = &pb.Event{
+			Id:           ev.ID,
+			Title:        ev.Title,
+			Time:         timestamppb.New(ev.Time),
+			Duration:     durationpb.New(ev.Duration),
+			Description:  description,
+			OwnerId:      ev.OwnerID,
+			NotifyBefore: notifyBefore,
+		}
+	}
+	return result
+}
